b6a3d783900acea36a75b09945121735c1efc5a2
Masataka Mori <masataka.mori20@gmail.com>
2017-08-06 18:03:58 +0900

    forgot to init the hashed array for each trial

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@ const int MAX_SIZE = 3000;
 
 int abs_value(int); // return absolute value of given integer
 
+void array_init(bool [], int n);
+
 void process_array(bool [], int); // store true if given int value exist
 bool test_array(bool [], int); // check the array if all indices have true or false
 
@@ -19,20 +21,17 @@ int main()
 	
 	bool hashed_array[MAX_SIZE]; // it will store 1 if there is the same index number with the difference of two succesive input numbers
 	string line;
-	int counter = 0;
 
 	while (getline(cin, line))
 	{
-
 		if (line == "") break;
 
-		if ( counter > 0)
-			cout << endl;
-		
 		istringstream iss(line);
 
-		int n = 0;	
+		int n = 0;
 		iss >> n; // first represents the number of digits in the line
+
+		array_init(hashed_array, n+1);
 		int current_value;
 		int previous_value;
 		for ( int i = 0; i < n; i++)
@@ -45,8 +44,8 @@ int main()
 
 				process_array(hashed_array, abs_value(diff));
 
-				//cout << "difference between " << current_value << "and"
-				//	<< previous_value << "is " << abs_value(diff) << endl;
+			//	cout << "difference between " << current_value << "and"
+			//		<< previous_value << "is " << abs_value(diff) << endl;
 			}
 
 			previous_value = current_value;
@@ -55,11 +54,10 @@ int main()
 		//display_array(hashed_array, n);
 
 		if (test_array(hashed_array, n))
-			cout << "Jolly";
+			cout << "Jolly" << endl;
 		else
-			cout << "Not Jolly"; 
+			cout << "Not Jolly" << endl; 
 
-		counter++;
 	}
 
 
@@ -103,6 +101,12 @@ void display_array(bool arr[], int n)
 
 }
 
+void array_init(bool arr[], int n)
+{
+
+	for (int i = 0; i < n+1; i++)
+		arr[i] = false;
+}
 
 
 
